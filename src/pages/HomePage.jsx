38def5cd860298d9c6951028fe946f57a9f54a13
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-06 17:54:54 -0300

    + showing product list

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,20 @@
+import { useEffect, useState } from "react"
 import { useAuth } from "../components/AuthProvider.jsx"
+import api from '../api.js'
+import { ProductList } from "../components/ProductList.jsx"
 
 export function HomePage () {
     const { user, loadingUser } = useAuth()
-    
+    const [ loadingProducts, setLoadingProducts ] = useState(true)
+    const [products, setProducts ] = useState([])
+    useEffect(() => {
+        api.get('/products')
+        .then(res => {
+            setProducts(res.data)
+            setLoadingProducts(false)
+        })
+    }, [])
+
     if (loadingUser) {
         return <></>
     }
@@ -11,6 +23,10 @@ export function HomePage () {
         <>
             <h1>Home Page</h1>
             <p>Welcome {user.username}</p>
+            {loadingProducts ? (<h2>Loading...</h2>) 
+            : 
+            <ProductList products={products}></ProductList>
+            }
         </>
     )
 }
