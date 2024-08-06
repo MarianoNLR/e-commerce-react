70392bee53f731634c2388d1b82a5a25013ba932
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-06 18:22:26 -0300

    + Footer component

---
+++ src/components/Layout/Footer/Footer.jsx
@@ -0,0 +1,29 @@
+import githubLogo from '../../../assets/logos/github-logo.png'
+import igLogo from '../../../assets/logos/ig-logo.png'
+import linkedinLogo from '../../../assets/logos/linkedin-logo.png'
+import './Footer.css'
+
+export function Footer () {
+    return (
+      <>
+        <div className="footer-wrapper">
+          <div className='footer-social-wrapper'>
+            <ul className='social-list'>
+              <li className='social-list-item'><a className='social-list-item-link' href='https://github.com/MarianoNLR' target='_blank'>
+              <img src={githubLogo} alt="" className='social-list-item_image'/></a>
+              </li>
+              <li className='social-list-item'><a className='social-list-item-link' href='https://www.instagram.com/marianolotero/' target='_blank'>
+              <img src={igLogo} alt="" className='social-list-item_image'/>
+              </a>
+              </li>
+              <li className='social-list-item'><a className='social-list-item-link' href='https://www.linkedin.com/in/mariano-lotero-rol%C3%B3n-26011124a/' target='_blank'>
+              <img src={linkedinLogo} alt="" className='social-list-item_image'/>
+              </a>
+              </li>
+            </ul>
+          </div>
+        <span>Designed and Developed by Mariano Lotero Rolón</span>
+        </div>
+      </>
+    )
+  }
