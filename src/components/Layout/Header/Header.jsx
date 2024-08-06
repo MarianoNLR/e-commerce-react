4693b22db90ef8e349f69c19625ff19c79f55bf5
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-06 18:22:54 -0300

    + Header Component

---
+++ src/components/Layout/Header/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css'
+export function Header () {
+    return (
+        <>
+            <div className="header-wrapper">
+                <a href="#">Logo</a>
+            </div>
+        </>
+    )
+}
