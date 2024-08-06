d560fdc4e694780acb5b40fb711f8036827c787b
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-06 18:21:16 -0300

    + using Layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import { LoginPage } from './pages/LoginPage'
 import { AuthProvider } from './components/AuthProvider.jsx'
 import './App.css'
 import { HomePage } from './pages/HomePage.jsx'
+import Layout from './components/Layout/Layout.jsx'
 
 function App() {
   return (
     <>
     <AuthProvider>
-        <Routes>
-          <Route index path='/login' element={<LoginPage />} />
-          <Route path='/' element={<HomePage />}></Route> 
-        </Routes>
+        <Layout>
+          <Routes>
+            <Route index path='/login' element={<LoginPage />} />
+            <Route path='/' element={<HomePage />}></Route> 
+          </Routes>
+        </Layout>
       </AuthProvider>
     </>
   )
