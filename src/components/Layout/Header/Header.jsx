252574b445275fab1273fcf3f9507532694f65e0
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-07 17:33:46 -0300

    + cart icon

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,12 +1,13 @@
 import './Header.css'
 import { Link } from 'react-router-dom'
-
+import cartIcon from '../../../assets/cart-icon/icons8-cart-50.png'
 export function Header () {
 
     return (
         <>
             <div className="header-wrapper">
                 <Link to='/'>Logo</Link>
+                <Link><img className='cart-image' src={cartIcon} alt="" /></Link>
             </div>
         </>
     )
