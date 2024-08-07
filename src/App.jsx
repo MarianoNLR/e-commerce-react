84da8679ee7f56c37b0bd27efaa40cdf737836bf
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-07 01:39:07 -0300

    + route for product view

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from 'react-router-dom'
-import { LoginPage } from './pages/LoginPage'
+import { LoginPage } from './pages/LoginPage/LoginPage.jsx'
 import { AuthProvider } from './components/AuthProvider.jsx'
 import './App.css'
-import { HomePage } from './pages/HomePage.jsx'
+import { HomePage } from '../src/pages/HomePage/HomePage.jsx'
 import Layout from './components/Layout/Layout.jsx'
+import { ProductView } from './pages/ProductView/ProductView.jsx'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route index path='/login' element={<LoginPage />} />
             <Route path='/' element={<HomePage />}></Route> 
+            <Route path='/products/:productId' element={<ProductView/>}></Route>
           </Routes>
         </Layout>
       </AuthProvider>
