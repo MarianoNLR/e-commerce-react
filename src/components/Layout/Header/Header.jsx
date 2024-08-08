c9212c9416ffb1ba2956aa7a40a978ce8ff07324
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-08 17:37:29 -0300

    + cart icon and control if there is user or not

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,14 +1,27 @@
 import './Header.css'
 import { Link } from 'react-router-dom'
 import cartIcon from '../../../assets/cart-icon/icons8-cart-50.png'
+import { useAuth } from '../../AuthProvider.jsx'
 export function Header () {
+    const {user, loadingUser} = useAuth()
+
+    if (loadingUser) {
+        return <></>
+    }
 
     return (
         <>
-            <div className="header-wrapper">
-                <Link to='/'>Logo</Link>
-                <Link><img className='cart-image' src={cartIcon} alt="" /></Link>
-            </div>
+            {user ? 
+                <div className="header-wrapper">
+                    <Link to='/'>Logo</Link>
+                    <Link to={`/cart/${user.id}`}><img className='cart-image' src={cartIcon} alt="" /></Link>
+                </div>
+                :
+                <div className="header-wrapper">
+                    <Link to='/'>Logo</Link>
+                </div>
+            }
+            
         </>
     )
 }
