f8e52711117ea389c502963266394eb0dc0b271b
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-09 18:26:30 -0300

    + filter panel component

---
+++ src/components/FilterPanel/FilterPanel.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react'
+import { FilterPanelOption } from '../FilterPanelOption/FilterPanelOption.jsx'
+import api from '../../api.js'
+import './FilterPanel.css'
+
+export function FilterPanel () {
+    const [categories, setCategories] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError ] = useState(null)
+
+    useEffect(() => {
+        const fetchCategories = async  () => {
+            await api.get('/category')
+            .then(res => {
+                setCategories(res.data.categories)
+                setLoading(false)
+            })
+            .catch(err => {
+                console.error(err)
+                setError(err)
+                setLoading(false)
+            })
+        }
+
+        fetchCategories()
+    }, [])
+
+    if (loading) {
+        return <>Cargando...</>
+    }
+
+    if (error) {
+        return <>Error al cargar las categorias</>
+    }
+
+    return (
+        <>
+            <h1>Filtros</h1>
+            <div className='filters-options-wrapper'>
+                {categories.map((item, index) => (
+                    <FilterPanelOption key={index} categoryId={item.id} name={item.name}></FilterPanelOption>
+                ))}
+                
+            </div>
+        </>
+    )
+}
