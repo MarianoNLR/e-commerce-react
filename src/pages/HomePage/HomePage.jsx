2475127275ed588f17b9feba2864926879dd92d3
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-09 18:29:58 -0300

    +´filter panel

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react"
 import { useAuth } from "../../components/AuthProvider.jsx"
-import api from '../../api.js'
+import api from "../../api.js"
 import { ProductList } from "../../components/ProductList/ProductList.jsx"
+import { FilterPanel } from "../../components/FilterPanel/FilterPanel.jsx"
 import './HomePage.css'
 
 export function HomePage () {
@@ -9,7 +10,7 @@ export function HomePage () {
     const [ loadingProducts, setLoadingProducts ] = useState(true)
     const [products, setProducts ] = useState([])
     useEffect(() => {
-        api.get('/products')
+        api.get('/products/')
         .then(res => {
             setProducts(res.data)
             setLoadingProducts(false)
@@ -25,7 +26,7 @@ export function HomePage () {
             <main>
                 <h1>Home Page</h1>
                 {user && <p>Welcome {user.username}</p>}
-                
+                <FilterPanel></FilterPanel>
                 {loadingProducts ? (<h2>Loading...</h2>) 
                 : 
                 <ProductList products={products}></ProductList>
