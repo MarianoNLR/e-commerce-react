1a6d1202264be65b08602beb3d84e49e2045a7ff
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-13 18:39:24 -0300

    using handle delete item and useCart to modify headers cart state

--- src/pages/CartView/CartViewPage.jsx
+++ src/pages/CartView/CartViewPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { useAuth } from '../../components/AuthProvider.jsx'
+import { useCart } from '../../components/CartProvider.jsx'
 import { useParams } from "react-router-dom"
 import api from '../../api.js'
 import { CartProductCard } from "../../components/CartProductCard/CartProductCard.jsx"
@@ -9,18 +10,18 @@ export function CartViewPage () {
     const [cart, setCart] = useState([])
     const [loadingCart, setLoadingCart] = useState(true)
     const {user, loadingUser} = useAuth()
-
+    const {setCartCount} = useCart()
+    //TODO make a different component for cart
     useEffect(() => {
         api.get(`/cart/${userId}`)
         .then(res => {
             setCart(res.data.cart)
             setLoadingCart(false)
-            console.log(res.data.cart)
         })
         .catch(err => {
             console.error(err)
         })
-    }, [userId])
+    }, [userId, loadingCart])
 
     if (loadingUser || !user) {
         return <></>
@@ -30,13 +31,35 @@ export function CartViewPage () {
         return <></>
     }
 
+    const handleDeleteItem = (e, product) => {
+        e.preventDefault()
+        setLoadingCart(true)
+        api.patch(`/cart/item/${product}`)
+        .then(res => {
+            //setCart(res.data.cart)
+            setCartCount(prev => prev - 1)
+            setLoadingCart(false)
+        })
+        .catch(err => {
+            console.error(err)
+        })
+    }
+
     return (
         <>
             <main>
                 <h1 className="cart-wrapper-title">Carrito de Compra</h1>
                 <div className="cart-details-wrapper">
                     {cart.items.map((item, index) => (
-                            <CartProductCard key={index} productId={item.product.id} name={item.product.name} price={item.product.price} quantity={item.quantity} imageURL={item.product.imageURL}></CartProductCard>
+                            <CartProductCard 
+                            key={index} 
+                            productId={item.product.id} 
+                            name={item.product.name} 
+                            price={item.product.price} 
+                            quantity={item.quantity} 
+                            imageURL={item.product.imageURL} 
+                            handleDeleteItem={handleDeleteItem}>
+                            </CartProductCard>
                     ))}
                 </div>
             </main>
