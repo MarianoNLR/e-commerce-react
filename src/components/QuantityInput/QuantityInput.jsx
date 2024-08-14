4d47417cea9a72929712e966f4079b121a87c10e
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-13 22:14:03 -0300

    + product quantity input component

---
+++ src/components/QuantityInput/QuantityInput.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export function QuantityInput ({ quantity, setQuantity, stock }) {
+    
+
+    const handleSubQuantity = (e) => {
+        e.preventDefault()
+        setQuantity(prev => {
+            const newQuantity = prev - 1
+            return newQuantity > 1 ? newQuantity : 1
+        })
+    }
+
+    const handleAddQuantity = (e) => {
+        e.preventDefault()
+        setQuantity(prev => {
+            const newQuantity = prev + 1
+            return newQuantity <= stock ? newQuantity : prev
+        })
+    }
+
+    return (
+        <>
+            <div className="quantity-input-wrapper">
+                <input type="button" className="quantity-input subtract" value="-" onClick={(e) => handleSubQuantity(e)} />
+                <span className="quantity-output">{quantity}</span>
+                <input type="button" className="quantity-input add" value="+" onClick={(e) => handleAddQuantity(e)}/>
+            </div>
+        </>
+    )
+}
