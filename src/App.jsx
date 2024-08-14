43a091eb90b475024c86cc58fc6955b94eedffe9
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-14 14:31:43 -0300

    Search product route

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { ProductView } from './pages/ProductView/ProductView.jsx'
 import { CartViewPage } from './pages/CartView/CartViewPage.jsx'
 import { CartProvider } from './components/CartProvider.jsx'
 import { CategoryProducts } from './pages/CategoryProducts/CategoryProducts.jsx'
+import { SearchProduct } from './pages/SearchProduct/SearchProduct.jsx'
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
               <Route path='/login' element={<LoginPage />} />
               <Route index path='/' element={<HomePage />}></Route> 
               <Route path='/products/product/:productId' element={<ProductView/>}></Route>
+              <Route path='/products/search/' element={<SearchProduct/>}></Route>
               <Route path='/cart/:userId' element={<CartViewPage />}></Route>
               <Route path='products/:categoryId' element={< CategoryProducts/>}></Route>
             </Routes>
