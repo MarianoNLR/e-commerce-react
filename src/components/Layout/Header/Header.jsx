5c882956c020b50c3bf45f7d0f8de397e84cc0cf
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-14 11:33:09 -0300

    using cart header component

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,9 +1,8 @@
 import './Header.css'
 import { Link } from 'react-router-dom'
-import cartIcon from '../../../assets/cart-icon/icons8-cart-50.png'
 import { useAuth } from '../../AuthProvider.jsx'
-import { useEffect, useState } from 'react'
 import { useCart } from '../../CartProvider.jsx'
+import { CartHeader } from '../../CartHeader/CartHeader.jsx'
 export function Header () {
     const {user, loadingUser} = useAuth()
     const { cartCount, loadingCartCount } = useCart()
@@ -17,11 +16,7 @@ export function Header () {
             {user ? 
                 <div className="header-wrapper">
                     <Link to='/'>Logo</Link>
-                    <div className='cart-icon-wrapper'>
-                        <Link to={`/cart/${user.id}`}><img className='cart-image' src={cartIcon} alt="" />
-                        </Link>
-                        <span className='cart-products-number'>{cartCount}</span>
-                    </div>
+                    <CartHeader></CartHeader>
                 </div>
                 :
                 <div className="header-wrapper">
