0d4639a7754a5427d276aea9fb68dc4ea6c58382
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-14 11:31:54 -0300

    + cart header component

---
+++ src/components/CartHeader/CartHeader.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom"
+import { useAuth } from "../AuthProvider.jsx"
+import { useCart } from "../CartProvider.jsx"
+import cartIcon from '../../assets/cart-icon/icons8-cart-50.png'
+export function CartHeader () {
+    const {user, loadingUser} = useAuth()
+    const { cartCount, loadingCartCount } = useCart()
+
+    if (loadingUser) {
+        return null
+    }
+
+    if (loadingCartCount) {
+        return <>Loading...</>
+    }
+
+    return (
+        <div className='cart-icon-wrapper'>
+            <Link to={`/cart/${user.id}`}><img className='cart-image' src={cartIcon} alt="" />
+            </Link>
+            <span className='cart-products-number'>{cartCount}</span>
+        </div>
+    )
+}
