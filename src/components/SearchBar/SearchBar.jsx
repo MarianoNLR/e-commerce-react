faa65379cd41aa5d54284c07cf6b65322384c50d
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-14 15:35:02 -0300

    search bar component

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import './SearchBar.css'
 
 export function SearchBar () {
     const [searchInput, setSearchInput] = useState('')
@@ -17,8 +18,12 @@ export function SearchBar () {
 
     return (
         <div className="search-bar-wrapper">
-            <input type="text" name="q" id="" placeholder="Buscar producto" onChange={(e) => handleSearchInputChange(e)}/>
-            <input type="button" value="Buscar" onClick={handleSearchClick}/>
+            <input className="search-bar-input" type="text" name="q" id="" placeholder="Buscar producto" onChange={(e) => handleSearchInputChange(e)}/>
+            <div className="search-bar-submit" onClick={handleSearchClick}>
+                <svg  width="30px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14.9536 14.9458L21 21M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                </svg>
+            </div>
         </div>    
     )
 }
