c59d91cdbedc92fad35aca53bd4add2d41fad65d
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-16 18:39:55 -0300

    redirect to home if user

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,9 +1,19 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { LoginForm } from "../../components/LoginForm/LoginForm.jsx"
 import { SignUpForm } from "../../components/SignUpForm/SignUpForm.jsx"
 import './LoginPage.css'
+import { useNavigate } from "react-router-dom"
+import { useAuth } from "../../components/AuthProvider.jsx"
 export function LoginPage () {
     const [showLoginForm, setShowLoginForm] = useState(true)
+    const user = useAuth()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (user) {
+          navigate('/')
+        }
+      }, [user, navigate])
 
     return (
         <>
