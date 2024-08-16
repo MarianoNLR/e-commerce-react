a7e25add463b6283af7be3d40f87e43dba82afff
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-15 21:01:40 -0300

    using form message

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react"
 import PropTypes from 'prop-types'
 import './SignUpForm.css'
+import { FormErrorMessage } from "../FormErrorMessage/FormErrorMessage"
 
 export function SignUpForm (props) {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
     const [confirmPassword, setConfirmPassword] = useState("")
+    const [formMessage, setFormMessage] = useState("")
     const onChangeUsername = (e) => {
         setUsername(e.target.value)
     }
@@ -20,9 +22,26 @@ export function SignUpForm (props) {
 
     const handleSubmitLogin = async (e) => {
         e.preventDefault()
-        console.log(username)
-        console.log(password)
-        console.log(confirmPassword)
+        if (username === '') {
+            setFormMessage('Debes ingresar un nombre de usuario!')
+            return
+        }
+
+        if (password === '') {
+            setFormMessage('Debes ingresar una contraseña!')
+            return
+        }
+
+        if (confirmPassword === '') {
+            setFormMessage('Debes confirmar la contraseña!')
+            return
+        }
+
+        if (password !== confirmPassword) {
+            setFormMessage('Las contraseña deben coincidir!')
+            return
+        }
+        
         try {
             //await login({username, password})
             console.log('Sesion Iniciada! Supuestamente.')
@@ -39,7 +58,8 @@ export function SignUpForm (props) {
                 <input type="text" name="username" id="username" placeholder="Usuario" onChange={(e) => onChangeUsername(e)}/>
                 <input type="password" name="password" id="password" placeholder="Contraseña" onChange={(e) => onChangePassword(e)} />
                 <input type="password" name="confirmPassword" id="confirm-password" placeholder="Confirmar Contraseña" onChange={(e) => onChangeConfirmPassword(e)} />
-                <input type="submit" value="Inciar Sesion" />
+                <FormErrorMessage message={formMessage}></FormErrorMessage>
+                <input type="submit" value="Registrarme" />
                 <div className="link-register-wrapper">
                     <p>Ya tienes una cuenta?</p>
                     <a href="#" onClick={() => props.setShowLoginForm(true)}>Iniciar Sesión</a>
