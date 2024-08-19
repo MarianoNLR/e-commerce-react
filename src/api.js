fe04dd2ab335c6d22082596fc992e1fcad3643b5
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-19 19:50:46 -0300

    endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,23 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://e-commerce-api-gpfg.onrender.com',
     withCredentials: true
 })
+
+api.interceptors.request.use(
+    (config) => {
+        const user = JSON.parse(window.localStorage.getItem('user'))
+        if (!user) return config
+        if (user.token) {
+            config.headers['authorization'] = `Bearer ${user.token}`
+        }
+
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+);
+
 export default api
