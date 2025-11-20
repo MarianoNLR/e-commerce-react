cebb67675649b7451dcfbad493a23a221f146e17
MarianoNLR <marianonehuenlr@gmail.com>
2025-11-20 00:33:34 -0300

    using mobile header detecting width with useMediaQuery

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,9 +4,12 @@ import { useAuth } from '../../hooks/useAuth.jsx'
 import { SidebarAdmin } from './SidebarAdmin/SidebarAdmin.jsx'
 import PropTypes from 'prop-types'
 import { useEffect } from 'react'
+import { useMediaQuery } from 'react-responsive'
+import { MobileHeader } from './Header/MobileHeader.jsx'
 
 export default function Layout(props) {
   const { user, loadingUser } = useAuth();
+  const isMobile = useMediaQuery({ query: '(max-width: 1300px)' });
 
   if (loadingUser) {
     return <></>;
@@ -14,15 +17,30 @@ export default function Layout(props) {
 
   return (
     <>
-      <Header></Header>
-      <div className='flex flex-1'>
-        {user?.role.includes('admin') && 
-          <SidebarAdmin></SidebarAdmin>
-        }
-        <main className='flex-1 min-h-10vh'>
-          {props.children}
-        </main>
-      </div>
+      {isMobile ? (
+        <>
+          <MobileHeader></MobileHeader>
+          <div className='flex flex-1 py-20 mt-4'>
+            <main className='flex-1 min-h-10vh'>
+              {props.children}
+            </main>
+          </div>
+        </>
+      ) 
+      : 
+      (
+      <>
+        <Header></Header>
+          <div className='flex flex-1 py-20 mt-4'>
+            {user?.role.includes('admin') && 
+              <SidebarAdmin></SidebarAdmin>
+            }
+            <main className='flex-1 min-h-10vh '>
+              {props.children}
+            </main>
+          </div>
+      </>
+      )}
       <Footer></Footer>
     </>
   )
