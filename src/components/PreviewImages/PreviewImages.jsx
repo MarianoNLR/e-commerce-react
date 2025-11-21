15103007d197edfae9054abd868ade362aff5831
MarianoNLR <marianonehuenlr@gmail.com>
2025-11-21 19:58:24 -0300

    + preview images component

---
+++ src/components/PreviewImages/PreviewImages.jsx
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+
+export function PreviewImages({ images, onRemove }) {
+
+    // Eliminar imagen
+    const removeImage = (index) => {
+        if (images[index].preview && !images[index].exists) {
+            URL.revokeObjectURL(images[index].preview); // Limpiar memoria
+        }
+        const newImages = [...images];
+        newImages.splice(index, 1);
+        onRemove([...newImages]);
+    };
+
+    return (
+        <>
+        {images.length > 0 && (
+                <div className="images-preview-container">
+                    {images.map((image, index) => (
+                        <div key={index} className="image-preview-wrapper">
+                            <img 
+                                src={image.preview} 
+                                alt={`Preview ${index}`}
+                                className="image-preview"
+                            />
+                            <button
+                                type="button"
+                                onClick={() => removeImage(index)}
+                                className="remove-image-btn"
+                            >
+                                ✕
+                            </button>
+                        </div>
+                    ))}
+                </div>   
+            )}
+        </>    
+    )
+}
+
+PreviewImages.propTypes = {
+    images: PropTypes.array.isRequired,
+    onRemove: PropTypes.func.isRequired
+};
