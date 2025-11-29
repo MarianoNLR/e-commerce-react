79f9cdb425a92ca960923d68004354bbf6e165db
MarianoNLR <marianonehuenlr@gmail.com>
2025-11-28 23:48:37 -0300

    styles

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -14,19 +14,24 @@ export function ProductList ({products, setProducts, loadingProducts}) {
         <>  
             <div className="product-list-main">
             <ProductSortCriteria products={products} setProducts={setProducts}></ProductSortCriteria>
-                <div className="product-list-wrapper">
-                    
                     {products.products.length === 0 ? 
                         <h3 className="not-elements-found-text">No hay Productos Disponibles</h3>
                         :
-                        products.products.map((item, index) => (
-                            <ProductCard key={index} productId={item.id} index={item.index} name={item.name} price={item.price} quantity={item.quantity} imageURL={item.imageURL}></ProductCard>
-                        ))
-                    }
-
-                        
+                        <div className="product-list-wrapper">
+                            {products.products.map((item, index) => (
+                                <ProductCard 
+                                key={index} 
+                                productId={item.id} 
+                                index={item.index} 
+                                name={item.name} 
+                                price={item.price} 
+                                quantity={item.quantity} 
+                                imagesURLs={item.imagesURLs}>
+                                </ProductCard>
+                            ))}
+                        </div>
+                    }      
                 </div>
-            </div>
         </>
     )
 }
