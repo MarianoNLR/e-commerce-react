098f03b56e38eac2cd330ef586fbb3aca276ff5d
MarianoNLR <marianonehuenlr@gmail.com>
2026-01-17 18:51:01 -0300

    backend url from .env and change token to string in local storage

--- src/api.js
+++ src/api.js
@@ -1,19 +1,20 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
 })
 
 api.interceptors.request.use(
     (config) => {
-        const token = JSON.parse(window.localStorage.getItem('access_token'))
+        const token = window.localStorage.getItem('access_token')
         if (!token) return config
-        config.headers['authorization'] = `Bearer ${token}`
+        config.headers.Authorization = `Bearer ${token}`
 
         return config
     },
     (error) => {
+        localStorage.removeItem('access_token')
         return Promise.reject(error)
     }
 );
