93c09f4e583f22269eaf27e33607573e3a988dbd
MarianoNLR <marianonehuenlr@gmail.com>
2026-04-24 02:20:40 -0300

    feat: remove CheckoutSuccessPage and CheckoutFailurePage components

--- src/pages/Checkout/CheckoutFailurePage.jsx
+++
@@ -1,8 +0,0 @@
-export function CheckoutFailurePage() {
-    return (
-        <div className="checkout-failure-page">
-            <h1>¡Lo sentimos!</h1>
-            <p>Hubo un problema al procesar tu pedido. Por favor, intenta nuevamente.</p>
-        </div>
-    )
-}
--- src/pages/Checkout/CheckoutSuccessPage.jsx
+++
@@ -1,8 +0,0 @@
-export function CheckoutSuccessPage() {
-    return (
-        <div className="checkout-success-page">
-            <h1>¡Gracias por tu compra!</h1>
-            <p>Tu pedido ha sido procesado exitosamente.</p>
-        </div>
-    )
-}
