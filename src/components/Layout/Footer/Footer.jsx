8e1a932785a6b7bcb89e35415e4b636e11522d1f
MarianoNLR <marianonehuenlr@gmail.com>
2026-05-06 17:12:55 -0300

    update: social links styles

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -9,13 +9,13 @@ export function Footer () {
         <div className="footer-wrapper">
           <div className='footer-social-wrapper'>
             <ul className='social-list'>
-              <li className='social-list-item'><a className='social-list-item-link' href='https://github.com/MarianoNLR' target='_blank'>
+              <li className='social-list-item github-logo'><a className='social-list-item-link' href='https://github.com/MarianoNLR' target='_blank'>
               <img src={githubLogo} alt="" className='social-list-item_image'/></a>
               </li>
-              <li className='social-list-item'><a className='social-list-item-link' href='https://www.instagram.com/marianolotero/' target='_blank'>
+              {/* <li className='social-list-item'><a className='social-list-item-link' href='' target='_blank'>
               <img src={igLogo} alt="" className='social-list-item_image'/>
               </a>
-              </li>
+              </li> */}
               <li className='social-list-item'><a className='social-list-item-link' href='https://www.linkedin.com/in/mariano-lotero-rol%C3%B3n-26011124a/' target='_blank'>
               <img src={linkedinLogo} alt="" className='social-list-item_image'/>
               </a>
